24d859363d6fd44556032c127c8940b6857163ae
hemanthkatam <katamkv@gmail.com>
2019-11-28 15:49:31 +0530

    Fx converter implemented

---
+++ src/Constant.js
@@ -0,0 +1,156 @@
+export const currency = ["AUD", "CAD", "CNY", "CZK", "DKK", "EUR", "GBP", "JPY", "NOK", "NZD", "USD"];
+
+export const currencyJson = {
+    AUD: {
+        AUD: '1:1',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'USD',
+        DKK: 'USD',
+        EUR: 'USD',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'USD',
+        NZD: 'USD',
+        USD: 'D'
+
+    },
+    CAD: {
+        AUD: 'USD',
+        CAD: '1:1',
+        CNY: 'USD',
+        CZK: 'USD',
+        DKK: 'USD',
+        EUR: 'USD',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'USD',
+        NZD: 'USD',
+        USD: 'D'
+
+    },
+    CNY: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: '1:1',
+        CZK: 'USD',
+        DKK: 'USD',
+        EUR: 'USD',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'USD',
+        NZD: 'USD',
+        USD: 'Inv'
+
+    },
+    CZK: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: '1:1',
+        DKK: 'EUR',
+        EUR: 'Inv',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'EUR',
+        NZD: 'USD',
+        USD: 'EUR'
+
+    },
+    DKK: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'EUR',
+        DKK: '1:1',
+        EUR: 'Inv',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'EUR',
+        NZD: 'USD',
+        USD: 'EUR'
+
+    },
+    EUR: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'D',
+        DKK: 'D',
+        EUR: '1:1',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'D',
+        NZD: 'USD',
+        USD: 'D'
+
+    },
+    GBP: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'USD',
+        DKK: 'USD',
+        EUR: 'USD',
+        GBP: '1:1',
+        JPY: 'USD',
+        NOK: 'USD',
+        NZD: 'USD',
+        USD: 'D'
+    },
+    JPY: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'USD',
+        DKK: 'USD',
+        EUR: 'USD',
+        GBP: 'USD',
+        JPY: '1:1',
+        NOK: 'USD',
+        NZD: 'USD',
+        USD: 'Inv'
+
+    },
+    NOK: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'EUR',
+        DKK: 'EUR',
+        EUR: 'Inv',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: '1:1',
+        NZD: 'USD',
+        USD: 'EUR'
+    },
+    NZD: {
+        AUD: 'USD',
+        CAD: 'USD',
+        CNY: 'USD',
+        CZK: 'USD',
+        DKK: 'USD',
+        EUR: 'USD',
+        GBP: 'USD',
+        JPY: 'USD',
+        NOK: 'USD',
+        NZD: '1:1',
+        USD: 'D'
+
+    },
+    USD: {
+        AUD: 'Inv',
+        CAD: 'Inv',
+        CNY: 'D',
+        CZK: 'EUR',
+        DKK: 'EUR',
+        EUR: 'Inv',
+        GBP: 'Inv',
+        JPY: 'D',
+        NOK: 'EUR',
+        NZD: 'Inv',
+        USD: '1:1'
+
+    },
+};
---
+++ src/CurrencyConv.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './App.css';
+import { currency, currencyJson } from './Constant';
+
+export default class CurrencyConv extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            valueTo: "",
+            valueFrom: "",
+            selectedTo: "AUD",
+            selectedFrom: "AUD"
+        };
+        this.currencyPair = {
+            AUDUSD: 0.837,
+            CADUSD: 0.871,
+            USDCNY: 6.171,
+            EURUSD: 1.231,
+            GBPUSD: 1.568,
+            NZDUSD: 0.775,
+            USDJPY: 119.9,
+            EURCZK: 27.602,
+            EURDKK: 7.440,
+            EURNOK: 8.665
+        };
+    }
+
+    handleFromValue = (e) => {
+        e.preventDefault()
+        let valueFrom = e.target.value;
+        const resultvalue = this.getConverterdValues(this.state.selectedFrom, this.state.selectedTo, valueFrom);
+        const withdecimal = this.state.selectedTo !== 'JPY' ? parseFloat(resultvalue).toFixed(2) : parseFloat(resultvalue).toFixed(0);
+        this.setState({
+            valueFrom,
+            valueTo: withdecimal
+        })
+    }
+    handleSelectionFrom = (e) => {
+        let selectedFrom = e.target.value;
+        const resultvalue = this.getConverterdValues(selectedFrom, this.state.selectedTo, this.state.valueFrom);
+        const withdecimal = this.state.selectedTo !== 'JPY' ? parseFloat(resultvalue).toFixed(2) : parseFloat(resultvalue).toFixed(0);
+        this.setState({
+            selectedFrom,
+            valueTo: withdecimal
+        })
+    }
+    handleSelectionTo = (e) => {
+        let selectedTo = e.target.value;
+        const resultvalue = this.getConverterdValues(this.state.selectedFrom, selectedTo, this.state.valueFrom);
+        const withdecimal = selectedTo !== 'JPY' ? parseFloat(resultvalue).toFixed(2) : parseFloat(resultvalue).toFixed(0);
+        this.setState({
+            selectedTo,
+            valueTo: withdecimal
+        })
+    }
+
+    getConverterdValues = (fromValue, toValue, fromData) => {
+        if (currencyJson && currencyJson[fromValue] && currencyJson[fromValue][toValue]) {
+            let jsonResult = currencyJson[fromValue][toValue];
+            if (jsonResult === "1:1") {
+                return fromData;
+            }
+            else if (jsonResult === "D") {
+                const svalue = this.currencyPair[fromValue + toValue];
+                return fromData * svalue;
+            }
+            else if (jsonResult === "Inv") {
+                const svalue = this.currencyPair[toValue + fromValue];
+                return fromData * (1 / svalue);
+            }
+            else if (jsonResult !== '') {
+                const fromCal = this.getConverterdValues(fromValue, jsonResult, fromData);
+                const toCal = this.getConverterdValues(jsonResult, toValue, fromCal);
+                return toCal;
+            }
+        } else {
+            alert('Unable to find rate');
+            return 0;
+        }
+    }
+
+    render() {
+        let currencyValue = currency.map((x) => <option>{x}</option>)
+        return (
+            <div>
+                <h1>FX Converter</h1>
+                <label className="label">From</label>&nbsp;
+                <input style={{ height: "43px" }} type="number" value={this.state.valueFrom} onChange={(event) => this.handleFromValue(event)} />&nbsp;
+                <select style={{ height: "50px" }} onChange={(event) => this.handleSelectionFrom(event)} value={this.state.selectedFrom}>
+                    {currencyValue}
+                </select>&nbsp;
+                <label className="label">To</label>&nbsp;
+                <input style={{ height: "43px" }} type="number" value={this.state.valueTo} />&nbsp;
+                <select style={{ height: "50px" }} onChange={(event) => this.handleSelectionTo(event)} value={this.state.selectedTo}>
+                    {currencyValue}
+                </select>
+            </div>
+        )
+    }
+}
